8c9b1444933bb99eeb114020d63c11e7579bd1e8
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-06-15 17:46:21 -0400

    Add files via upload

---
+++ src/component/ToDo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Card, CardContent, Typography, Container, IconButton } from '@material-ui/core/';
+//import {Check, Delete} from '@material-ui/icons';
+
+const ToDo = ({title, crossTask, id, isCompleted, deleteTask}) => {
+    const delTask = () => deleteTask(id)
+    const markComplete = () => crossTask(id);
+    const todoStyle = isCompleted ? { textDecoration: "line-through"} : {textDecoration: "none"}
+	return (
+    <div>
+
+        <Container>
+            <Card variant="outlined" style={{marginTop: 35, background:"lightgray"}}>
+                <CardContent>
+                    <Typography variant="h5" component="h2" style={todoStyle}>
+                        <IconButton style={{float:"left"}} onClick={markComplete}>
+                            Complete Task
+                        </IconButton>
+                        {title}
+                        <IconButton style={{float:"right"}} onClick={delTask} >
+                            Remove Task
+                        </IconButton >
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Container>
+
+    </div>
+	
+	);
+};
+
+
+export default ToDo;
